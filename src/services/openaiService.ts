c8c4fc2717746c8f18096f4d67cce416cf5ad7ec
Banti-kumar <banti.kumar@crossml.com>
2025-04-28 20:55:04 +0530

    remove .env

--- src/services/openaiService.ts
+++ src/services/openaiService.ts
@@ -13,7 +13,11 @@ async function fetchRealNews(
   count = 10
 ): Promise<NewsArticle[]> {
   try {
-    const url = `/api/serp/search.json?engine=google_news&q=${encodeURIComponent(
+    // const url = `/api/serp/search.json?engine=google_news&q=${encodeURIComponent(
+    //   query
+    // )}&api_key=${import.meta.env.VITE_SERPAPI_KEY}&num=${count}`;
+
+    const url = `https://serpapi.com/search.json?engine=google_news&q=${encodeURIComponent(
       query
     )}&api_key=${import.meta.env.VITE_SERPAPI_KEY}&num=${count}`;
 
